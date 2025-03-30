4eae29de5a1784dbfad53570029afaa4c90318f1
me <mothership2002@naver.com>
2025-03-30 21:15:55 +0900

    git

--- src/main/java/horizon/HorizonApplication.java
+++ src/main/java/horizon/HorizonApplication.java
@@ -8,6 +8,7 @@ public class HorizonApplication {
     public static void main(String[] args) throws Exception {
         HorizonContextCoordinator coordinator = new HorizonContextCoordinator();
         coordinator.register(new NettyEngineContext());
+        // TODO multi protocol server
         coordinator.runAll(8080);
     }
 }
